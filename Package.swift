0b4a2547fc81b8aaa92e2bfd5ee6721257cdc918
curdicu <curdicu@gmail.com>
2025-02-28 21:47:27 +0800

    *

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ let package = Package(
         .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "6.0.0")),
         .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0")),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.21.0"),
-        .package(url: "https://github.com/curdicu/HsCryptoKit.git",  .branch("main")),
-        .package(url: "https://github.com/curdicu/HdWalletKit.git",  .branch("main")),
-        .package(url: "https://github.com/curdicu/HsToolKit.git",  .branch("main")),
-        .package(url: "https://github.com/curdicu/HsExtensions.git",  .branch("main")),
+        .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.2.1")),
+        .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMajor(from: "1.2.1")),
+        .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
     ],
     targets: [
         .target(
@@ -32,10 +32,10 @@ let package = Package(
                 .product(name: "GRDB", package: "GRDB.swift"),
                 "ObjectMapper",
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-                .product(name: "HsCryptoKit", package: "HsCryptoKit"),
-                .product(name: "HdWalletKit", package: "HdWalletKit"),
-                .product(name: "HsToolKit", package: "HsToolKit"),
-                .product(name: "HsExtensions", package: "HsExtensions"),
+                .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
+                .product(name: "HdWalletKit", package: "HdWalletKit.Swift"),
+                .product(name: "HsToolKit", package: "HsToolKit.Swift"),
+                .product(name: "HsExtensions", package: "HsExtensions.Swift"),
             ]
         ),
     ]
